a5d313a63c672f3a051b210e42f7a6b9c78752d1
piyush-pandit <piyushsanjay06@gmail.com>
2024-06-14 10:56:24 +0530

    new set commit

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -50,7 +50,7 @@ const ProductCard = () => {
   };
 
   return (
-    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4" style={{ marginTop: '4%'}}>
       {products?.map((product, index) => (
         <div key={index} className="col">
           <div className="card">
